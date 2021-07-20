2b644d4d7055093b94409d537c6d319daccc4c48
Paulus Schoutsen <balloob@gmail.com>
2021-07-20 23:43:26 +0000

    Update example

--- example.py
+++ example.py
@@ -25,22 +25,33 @@ async def main():
         pprint(dataclasses.asdict(estimate))
         print()
         print(f"energy_production_today: {estimate.energy_production_today}")
-        print(f"energy_production_tomorrow: {estimate.energy_production_tomorrow}")
-        print(f"power_production_now: {estimate.power_production_now}")
-        print(
-            f"power_production_next_hour: {estimate.power_production_at_time(estimate.now() + timedelta(hours=1))}"
-        )
         print(
             f"power_highest_peak_time_today: {estimate.power_highest_peak_time_today}"
         )
+        print(f"energy_production_tomorrow: {estimate.energy_production_tomorrow}")
         print(
             f"power_highest_peak_time_tomorrow: {estimate.power_highest_peak_time_tomorrow}"
         )
+        print()
+        print(f"power_production_now: {estimate.power_production_now}")
+        print(
+            f"power_production in 1 hour: {estimate.power_production_at_time(estimate.now() + timedelta(hours=1))}"
+        )
+        print(
+            f"power_production in 6 hours: {estimate.power_production_at_time(estimate.now() + timedelta(hours=6))}"
+        )
+        print(
+            f"power_production in 12 hours: {estimate.power_production_at_time(estimate.now() + timedelta(hours=12))}"
+        )
+        print(
+            f"power_production in 24 hours: {estimate.power_production_at_time(estimate.now() + timedelta(hours=24))}"
+        )
+        print()
+        print(f"energy_current_hour: {estimate.energy_current_hour}")
         print(f"energy_production_next_hour: {estimate.sum_energy_production(1)}")
         print(f"energy_production_next_6hours: {estimate.sum_energy_production(6)}")
         print(f"energy_production_next_12hours: {estimate.sum_energy_production(12)}")
         print(f"energy_production_next_24hours: {estimate.sum_energy_production(24)}")
-        print(f"energy_current_hour: {estimate.energy_current_hour}")
         print(f"timezone: {estimate.timezone}")
         print(forecast.ratelimit)
 
