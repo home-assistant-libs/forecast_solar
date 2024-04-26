b8c7a9135a73fa7d02974b2cb2c4ddb1f98a323b
Klaas Schoute <klaas_schoute@hotmail.com>
2024-04-26 13:37:07 +0200

    Move package code to src folder

---
+++ src/forecast_solar/__init__.py
@@ -0,0 +1,18 @@
+"""Asynchronous Python client for the Forecast.Solar API."""
+
+from .exceptions import ForecastSolarError, ForecastSolarConnectionError, ForecastSolarConfigError, ForecastSolarAuthenticationError, ForecastSolarRequestError, ForecastSolarRatelimit
+from .models import Estimate, AccountType, Ratelimit
+from .forecast_solar import ForecastSolar
+
+__all__ = [
+    "AccountType",
+    "Estimate",
+    "ForecastSolar",
+    "ForecastSolarAuthenticationError",
+    "ForecastSolarConfigError",
+    "ForecastSolarConnectionError",
+    "ForecastSolarError",
+    "ForecastSolarRatelimit",
+    "ForecastSolarRequestError",
+    "Ratelimit",
+]
