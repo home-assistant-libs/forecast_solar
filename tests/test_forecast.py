9ab0257a13fe17ef349c45d52cdcdd67620e038b
J. Nick Koston <nick@koston.org>
2025-05-08 11:23:47 -0500

    fixes

--- tests/test_forecast.py
+++ tests/test_forecast.py
@@ -6,8 +6,6 @@ from typing import Any
 from unittest.mock import patch
 
 import pytest
-from aiodns import DNSResolver
-from aiodns.error import DNSError
 from aiohttp import ClientSession
 from aresponses import ResponsesMockServer
 
